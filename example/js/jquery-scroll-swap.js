1f8568b5db20075e14b88bc3f8220981af8ad9cc
Jeremy Rue <jrue@users.noreply.github.com>
2022-03-22 23:40:53 -0700

    Overhaul

--- example/js/jquery-scroll-swap.js
+++ example/js/jquery-scroll-swap.js
@@ -30,52 +30,131 @@ https://creativecommons.org/licenses/by-nc-sa/4.0/
     };
 
      
-     //set the data, find offset top of each element. Re-run whenever window resizes.
-     base.findBreakPoints = function(){
+    //set the data, find offset top of each element. Re-run whenever window resizes.
+    base.findBreakPoints = function(){
        
-       //reset the viewport height if they resize
-       base.trueHeight = base.isMobile ? window.innerHeight * base.options.triggerFromTop : $(window).height() * base.options.triggerFromTop;
+      //reset the viewport height if they resize
+      base.trueHeight = base.isMobile ? window.innerHeight * base.options.triggerFromTop : $(window).height() * base.options.triggerFromTop;
       
-       //empty out the data variable
-       base.data.length = 0;
-       base.$el.find("[" + base.options.dataImageAttribute + "]").each(function(i,d){
-         base.data.push({elm:$(d), image:$(d).data('image'), offset: Math.round($(d).offset().top)});
-       });
-     }
-     
-     base.addPlaceHolder = function(){
-       
-       base.$el.find("[" + base.options.dataImageAttribute + "]").each(function(i,d){
-         let placeHolder = $("<div />").css({
-           position: "absolute",
-           height: "100%",
-           width: "100%",
-           top: 0,
-           left: 0,
-           zIndex: -1 - Number(i),
-           backgroundImage: "url(" + $(d).data('image') + ")",
-           backgroundAttachment: "fixed",
-           backgroundRepeat: "no-repeat",
-           backgroundSize: "cover",
-           backgroundPosition: "center",
-           transition: "opacity " + base.options.speed + "s ease-out",
-           opacity: 0
-         }).addClass("scrollBackgroundHolder");
+      //empty out the data variable
+      base.data.length = 0;
+      base.$el.find("[ data-" + base.options.dataImageAttribute + "]").each(function(i,d){
+        base.data.push({elm:$(d), image:$(d).data('image'), offset: Math.round($(d).offset().top)});
+      });
+    }
+    
 
-         placeHolder.appendTo(base.$el);
-       });
-       base.findBreakPoints();
+    base.addPlaceHolder = function(){
+
+      //unless they want to do their own CSS stuff, we need the container position relative.
+      if(base.options.noCSS == false){
+        base.$el.css({
+          position:'relative',
+          zIndex:1,
+          overflow:'hidden',
+          width:'100%',
+          height:'100%',
+          background: 'none'
+        });
+      }
        
-     }
+      //base.$el.find("[" + base.options.dataImageAttribute + "]").each(function(i,d){
+      $('> *', base.$el).each(function(i,d){
+
+
+        const image = $(d).data(base.options.dataImageAttribute);
+        let placeHolder;
+        
+      
+        //create placeholder divs for each image, background position fixed.
+        if(typeof image !== 'undefined'){
+
+          placeHolder = $("<div />").css({
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            top: 0,
+            left: 0,
+            zIndex: 999 - Number(i),
+            backgroundImage: "url(" + image + ")",
+            backgroundAttachment: "fixed",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            transition: "opacity " + base.options.speed + "s ease-out",
+            opacity: 0
+          }).addClass("scrollBackgroundHolder");
+
+        }
+
+
+        //Make sure the text is on top. $(d) are the p tags, first level child elements,
+        //regardless if they have a data element or not. 
+        if(base.options.noCSS == false){
+
+          $(d).css({
+            position:'relative',
+            zIndex: 1000,
+            backgroundColor:'transparent',
+            marginTop: base.options.spreadDistance,
+            marginBottom: base.options.spreadDistance,
+            width: base.options.width,
+            padding: '1.3rem',
+            backgroundColor: base.options.backgroundColor
+          });
+
+        }
+
+
+        //default center text, or if they explicitly say data-position="center"
+        if(typeof $(d).data('position') == 'undefined' || $(d).data('position').toLowerCase() == 'center'){
+          $(d).css({
+            marginLeft: 'auto',
+            marginRight: 'auto'
+          });
+        }
+
+
+        //left justify text if they have data-position="left"
+        if(typeof $(d).data('position') != 'undefined' && $(d).data('position').toLowerCase() == 'left'){
+          $(d).css({
+            marginLeft: '5%',
+            marginRight: 'auto',
+            width: base.options.width
+          });
+        }
+
+        //left justify text if they have data-position="left"
+        if(typeof $(d).data('position') != 'undefined' && $(d).data('position').toLowerCase() == 'right'){
+          $(d).css({
+            marginLeft: 'auto',
+            marginRight: '5%',
+            width: base.options.width
+          });
+        }
+
+        //if they have a data-image attribute, append the placeholder background image
+        if(typeof image !== 'undefined'){
+          placeHolder.appendTo(base.$el);
+        }
+
+      });
+      base.findBreakPoints();
+       
+    }
 
     // Run initializer
     base.init();
   };
 
   $.scrollSwap.defaultOptions = {
-    dataImageAttribute: 'data-image',
+    dataImageAttribute: 'image',
     triggerFromTop: 0.75,
-    speed: 0.5
+    speed: 0.5,
+    noCSS: false,
+    spreadDistance: "75vh",
+    backgroundColor: 'rgba(255,255,255,0.85)',
+    width: '50%'
   };
 
   $.fn.scrollSwap = function(options){
@@ -97,10 +176,9 @@ https://creativecommons.org/licenses/by-nc-sa/4.0/
       $(window).on('scroll', function(){
         
         let scrollTop = $(window).scrollTop() + scroll.trueHeight;
-          
 
         scroll.data.forEach(function(d, i){
-          
+
           //if it's after the last one
           if(scrollTop > scroll.data[scroll.data.length - 1].offset){
             $(".scrollBackgroundHolder").not(":nth-child(" + (scroll.data.length - 1) + ")").css("opacity", 0);
